ad5f3988f3285ad5722a13722a5715c792686492
Alexei Vinogradov <alexei@vinogradoff.de>
2017-02-20 00:24:04 +0100

    updated test to PO

--- src/test/java/com/seleniumcamp/selenide101/showcase/AutocompleteTest.java
+++ src/test/java/com/seleniumcamp/selenide101/showcase/AutocompleteTest.java
@@ -1,10 +1,9 @@
 package com.seleniumcamp.selenide101.showcase;
 
-import com.codeborne.selenide.*;
+import com.seleniumcamp.selenide101.showcase.pages.*;
 import org.junit.*;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 /**
@@ -19,14 +18,11 @@ public class AutocompleteTest {
 
   @Test
   public void autocompleteShowsUsStates() {
-    SelenideElement stateInput = $("md-autocomplete[placeholder*='US state'] input");
-    SelenideElement autocompleteBox = $(".md-autocomplete-suggestions");
-
-    stateInput.setValue("V");
-    autocompleteBox.shouldHave(text("Virginia"))
+    AutocompletePage.fill("V");
+    AutocompletePage.autocompleteBox.shouldHave(text("Virginia"))
             .shouldHave(text("Vermont"))
             .shouldNotHave(text("Alaska"));
-    autocompleteBox.find(byText("irginia")).click();
-    stateInput.shouldHave(attribute("value", "Virginia"));
+    AutocompletePage.selectFromAutocompleteBox("irginia");
+    AutocompletePage.stateInput.shouldHave(value("Virginia"));
   }
 }

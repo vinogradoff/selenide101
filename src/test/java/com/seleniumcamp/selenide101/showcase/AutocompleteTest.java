4a43076978a13a4a0d3442937ac3d320ae22e7a5
Alexei Vinogradov <alexei@vinogradoff.de>
2017-02-19 23:27:22 +0100

    better configurations use

--- src/test/java/com/seleniumcamp/selenide101/showcase/AutocompleteTest.java
+++ src/test/java/com/seleniumcamp/selenide101/showcase/AutocompleteTest.java
@@ -1,29 +1,19 @@
 package com.seleniumcamp.selenide101.showcase;
 
-import com.codeborne.selenide.*;
 import org.junit.*;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-import static java.lang.System.*;
 
 /**
  * @author Alexei Vinogradov
  */
 public class AutocompleteTest {
 
-  @BeforeClass
-  public static void setup() {
-    Configuration.timeout = 5000;
-    Configuration.browser = "chrome";
-    setProperty("webdriver.chrome.driver", "/Users/Shared/selenium/chromedriver-2.27");
-    Configuration.startMaximized = false;
-  }
-
   @Test
   public void autocompleteShowsUsStates(){
-    open("https://material.angularjs.org/latest/demo/autocomplete");
+    open("/demo/autocomplete");
     $("md-autocomplete[placeholder*='US state'] input").setValue("V");
     $(".md-autocomplete-suggestions").shouldHave(text("Virginia"))
             .shouldHave(text("Vermont"))

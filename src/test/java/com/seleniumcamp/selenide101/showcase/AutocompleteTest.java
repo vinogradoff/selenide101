e5c7ef2bcb554297171de03a6c07a37627f8e471
Alexei Vinogradov <alexei@vinogradoff.de>
2017-02-19 20:51:50 +0100

    initial showcase

---
+++ src/test/java/com/seleniumcamp/selenide101/showcase/AutocompleteTest.java
@@ -0,0 +1,14 @@
+package com.seleniumcamp.selenide101.showcase;
+
+import org.junit.*;
+
+/**
+ * @author Alexei Vinogradov
+ */
+public class AutocompleteTest {
+
+  @Test
+  public void autocompleteShowsUsStates(){
+
+  }
+}

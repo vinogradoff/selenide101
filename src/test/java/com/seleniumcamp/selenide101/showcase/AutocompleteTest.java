df0457b7dfd58fb8bf1611f1b0a2103c7cfafdf4
Alexei Vinogradov <alexei@vinogradoff.de>
2017-02-19 21:13:49 +0100

    autocomplete testcase initial solution

--- src/test/java/com/seleniumcamp/selenide101/showcase/AutocompleteTest.java
+++ src/test/java/com/seleniumcamp/selenide101/showcase/AutocompleteTest.java
@@ -2,6 +2,10 @@ package com.seleniumcamp.selenide101.showcase;
 
 import org.junit.*;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
 /**
  * @author Alexei Vinogradov
  */
@@ -9,6 +13,13 @@ public class AutocompleteTest {
 
   @Test
   public void autocompleteShowsUsStates(){
+    open("https://material.angularjs.org/latest/demo/autocomplete");
+    $("md-autocomplete[placeholder*='US state'] input").setValue("V");
+    $(".md-autocomplete-suggestions").shouldHave(text("Virginia"))
+            .shouldHave(text("Vermont"))
+            .shouldNotHave(text("Alaska"));
+    $(".md-autocomplete-suggestions").find(byText("irginia")).click();
+    $("md-autocomplete[placeholder*='US state'] input").shouldHave(value("Virginia"));
 
   }
 }

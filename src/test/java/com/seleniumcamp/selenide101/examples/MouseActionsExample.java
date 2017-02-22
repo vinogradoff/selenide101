e90d50a04d32045c6639241fcdc7ae325ee14ba7
Alexei Vinogradov <alexei@vinogradoff.de>
2017-02-22 15:40:32 +0100

    added mouse example (not ready yet)

---
+++ src/test/java/com/seleniumcamp/selenide101/examples/MouseActionsExample.java
@@ -0,0 +1,46 @@
+package com.seleniumcamp.selenide101.examples;
+
+import org.junit.*;
+import org.openqa.selenium.*;
+
+import static com.codeborne.selenide.Selenide.*;
+
+/**
+ * @author Alexei Vinogradov
+ */
+public class MouseActionsExample {
+
+  @Test
+  public void basicActions() {
+    $("").click();
+    $("").contextClick(); //right click
+    $("").doubleClick();
+    $("").hover(); //mouse over
+  }
+
+  @Test
+  public void dragAndDrop() {
+    // Doesn't work with HTML5 draggable at this time (Selenium Bug)
+    $("").dragAndDropTo($("element"));
+  }
+
+  @Test
+  public void otherActions() {
+
+    $("").scrollTo(); //scrolling to element
+
+    //Selenide.actions() -> Selenium Actions
+    actions().moveByOffset(10, 500)
+            .clickAndHold()
+            .moveByOffset(-10, -500)
+            .release()
+            .perform();
+
+    //keyboard actions are also there
+    actions().keyDown(Keys.ALT)
+            .sendKeys("P")
+            .keyUp(Keys.ALT);
+
+  }
+
+}

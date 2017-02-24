d487cf816b39cf1c8b1006726ba0ce448e1333d6
Alexei Vinogradov <alexei@vinogradoff.de>
2017-02-24 14:00:50 +0200

    VideoRecorderExample added

---
+++ src/test/java/com/seleniumcamp/selenide101/examples/VideoRecorderExample.java
@@ -0,0 +1,39 @@
+package com.seleniumcamp.selenide101.examples;
+
+import com.automation.remarks.junit.*;
+import com.automation.remarks.video.annotations.*;
+import org.junit.*;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+/**
+ * @author Alexei Vinogradov
+ */
+public class VideoRecorderExample {
+
+  // from http://github.com/SergeyPirogov/video-recorder-java
+  // see documentation for options available
+  @Rule
+  public VideoRule videoRule = new VideoRule();
+
+  //video is only saved, if the test failing
+  @Video
+  @Test
+  public void checkboxTest() {
+    open("https://the-internet.herokuapp.com/checkboxes");
+
+    $("[type=checkbox]").click();
+    $("[type=checkbox]").shouldBe(checked);
+
+    $("[type=checkbox]").setSelected(true);
+    $("[type=checkbox]").shouldNotBe(checked); //this should fail
+
+
+    $("[type=checkbox]", 1).setSelected(false);
+    $("[type=checkbox]", 1).shouldNotBe(checked);
+
+
+  }
+
+}
